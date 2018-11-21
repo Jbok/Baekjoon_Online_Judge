c93f7d48e3a338ab76319cace75e336381f4d4cc
Jbok <0614bok@gmail.com>
2018-11-21 23:04:27 +0900

    2352
    LIS
    if just use dynamic programming algorithm, it have n^2 time complexity.
    So, use lower bound having nlogn time complexity

--- 2300/2352.c
+++ 2300/2352.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 
+int lowBound(int arr[], int end, int value){
+    
+    int start = 0;
+    int mid;
+
+    while(1){
+        if (start > end){
+            return start;
+        }
+        mid = (start + end)/2;
+        if (arr[mid] == value){
+            return mid;
+        }else if (arr[mid] < value){
+            start = mid + 1;
+        }else{
+            end = mid -1;
+        }
+
+
+    }
+
+    return mid;
+}
+
 int main(){
     int n;
     scanf("%d", &n);
 
     int port[40000];
-    int dp[40000];
     for (int i = 0; i < n; i++){
         scanf("%d", &port[i]);
     }
 
-    for (int i = 0; i < n; i++){
-        dp[i] = 1;
-    }
-
-    for (int i = 1; i < n; i++){
-        for (int j = 0; j < i; j++){
-            if ((port[i] > port[j]) && (dp[i] < dp[j]+1)){
-                dp[i] = dp[j]+1;
-            }
-        }
-    }
-    
-    int resultMax = 0;
-    for (int i = 0; i < n; i++){
-        if (resultMax < dp[i]){
-            resultMax = dp[i];
+    int dp[40000];
+    int ii = 0;
+    dp[ii] = port[0];
+    for (int i = 1; i <= n; i++){
+        if (port[i] > dp[ii])
+            dp[++ii] = port[i];
+        else{
+            int index = lowBound(dp, ii-1, port[i]);
+            dp[index] = port[i];
         }
     }
 
-    printf("%d", resultMax);
+    printf("%d", ii+1);
 }
