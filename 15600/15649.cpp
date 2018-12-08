4914713a15de76f9daa8729f8a74e016fac632d6
Jbok <0614bok@gmail.com>
2018-12-08 21:18:40 +0900

    15649
    Make Permutation
    Use scanf/printf rather than cin/cout becuase of speed.

--- 15600/15649.cpp
+++ 15600/15649.cpp
@@ -1,18 +1,57 @@
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int main(){
+void permutation(int n, int r, int index, int height, int arr[], int output[], int flag[]){
 
-    int n, m;
-    cin >> n >> m;
+    int temparr[9] = {0, };
+    for (int i = 0; i < 9; i++){
+        temparr[i] = flag[i];
+    }
 
-    int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    output[height] = arr[index];
+    temparr[index]++;
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cout << arr[i] << ' ' << arr[j] << endl;
+
+    int temp = 0;
+    for (int i = 1; i <= n; i++){
+        if (flag[i] > 1){
+            return;
+        }
+        else if (flag[i] == 1){
+            temp++;
         }
     }
+
+    if (temp == r){
+        for (int i = 1; i <= r; i++){
+            printf("%d ", output[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    if (height > n)
+        return;
+
+    permutation(n, r, 1, height+1, arr, output, temparr);
+    
+    if (index != n){
+        permutation(n, r, index+1, height, arr, output, flag);
+    }
+}
+
+int main(){
+
+    int n, r;
+    scanf("%d", &n);
+    scanf("%d", &r);
+
+    int arr[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    int output[9] = {0, };
+    int flag[9] = {0, };
+
+    permutation(n, r, 1, 1, arr, output, flag);
+
 
 }
