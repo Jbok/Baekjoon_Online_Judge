6b52c3dce71e71b539bddc5a9223628bcd6757b4
Jbok <0614bok@gmail.com>
2018-12-29 18:47:12 +0900

    N x M (9, 10, 11)

---
+++ 15600/15665.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+void Permutation(int n, int m, int depth, int arr[], int result[], int flag[])
+{
+    if (m == depth)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d ", result[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    int depthArr[10001] = {0, };
+
+    for (int i = 0; i < n; i++)
+    {
+            if (depthArr[arr[i]] == 0)
+            {
+                result[depth] = arr[i];
+                depthArr[arr[i]] = 1;
+                flag[i] = 1;
+                Permutation(n, m, depth+1, arr, result, flag);
+                flag[i] = 0;
+            }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            int temp;
+            if (arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    int *result = new int[m];
+    int *flag = new int[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        flag[i] = 0;
+    }
+    Permutation(n, m, 0, arr, result, flag);
+
+}
