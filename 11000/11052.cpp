e04eea46aebedc77659ba2bbb3c443efbc74ba31
Jbok <0614bok@gmail.com>
2018-12-31 11:30:14 +0900

    11052
    Use DP

---
+++ 11000/11052.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin >> n;
+
+    int price[1001] = {0, };
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> price[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            int temp = price[j] + price[i-j];
+            if (price[i] < temp)
+                price[i] = temp;
+        }
+    }
+
+    cout << price[n] << endl;
+}
+
