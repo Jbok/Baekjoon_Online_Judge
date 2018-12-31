09fb46f1836089ea3c8f32d11532ea7da4a03df4
Jbok <0614bok@gmail.com>
2018-12-31 11:10:43 +0900

    11726 11727
    2xN Tileing use DP

---
+++ 11700/11727.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n+1];
+
+    arr[0] = 1;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = (2*arr[i-2])%10007 + arr[i-1]%10007;
+    }
+
+    cout << arr[n] % 10007 << endl;
+}
