68881e717d10598d527fa3d068dc33535d965fdd
Jbok <0614bok@gmail.com>
2019-01-01 22:18:03 +0900

    2193
    Use DP.
    check Integer range

---
+++ 2100/2193.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    long long arr[100][2] = { {0, }, };
+
+    arr[0][0] = 0;
+    arr[0][1] = 1;
+
+    for (int i = 1; i < 90; i++)
+    {
+        arr[i][0] = arr[i-1][0] + arr[i-1][1];
+        arr[i][1] = arr[i-1][0];
+    }
+    
+    cout << arr[n-1][0] + arr[n-1][1] << endl;
+}
