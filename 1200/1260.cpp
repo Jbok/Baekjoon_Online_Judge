3401cd427b120ad559b2e29a87a8d8791ff187bb
Jbok <0614bok@gmail.com>
2019-01-14 11:43:36 +0900

    1260 Make DFS, BFS
    use sort

--- 1200/1260.cpp
+++ 1200/1260.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <algorithm>
 
 using namespace std;
 
-vector<vector<int>>int v;
+vector< vector<int> > v;
 int flag[1001] = { 0, };
 
 void dfs(int now)
@@ -18,17 +20,15 @@ void dfs(int now)
         if (!flag[next])
             dfs(next);
     }
-
-    flag[now] = 0;
 }
 
 int main()
 {
-    int n, m, v;
+    int n, m, vStart;
     cin >> n >> m >> vStart;
+    v.resize(n+1);
 
-
-    for (int i = 0 ; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
         int x, y;
         cin >> x >> y;
@@ -37,5 +37,43 @@ int main()
         v[y].push_back(x);        
     }
 
+    for (int i = 0; i <= n; i++)
+    {
+        sort(v[i].begin(), v[i].end());
+    }
+
     dfs(vStart);
+
+    for (int i = 0; i <= n; i++)
+        flag[i] = 0;
+
+    cout << endl;
+
+    // BFS
+    deque<int> dq;
+    dq.push_back(vStart);
+    flag[vStart] = 1;
+
+    while (1)
+    {
+        if (dq.empty())
+            break;
+
+        int now = dq.front();
+        dq.pop_front();
+
+        printf("%d ", now);
+
+        for (int i = 0; i < v[now].size(); i++)
+        {
+            int next = v[now][i];
+           
+            if (!flag[next])
+            {
+                dq.push_back(next);
+                flag[next] = 1;
+            }
+        }
+    }
+
 }
