fe02a43f154af0d6db422c15eb4b1db882ee475a
Jbok <0614bok@gmail.com>
2019-01-22 00:28:09 +0900

    2529 Brute Force
    Use long long type

--- 2500/2529.cpp
+++ 2500/2529.cpp
@@ -1,58 +1,95 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
-int main(){
-    int k;
-    cin >> k;
+char minarr[11] = { 0, };
+char maxarr[11] = { 0, };
+char str[11];
 
-    int* inequality = new int[k];
-    for (int i = 0; i < k; i++){
-        cin >> inequality[i];
-    }
-
-    int* arr = new int[k+1];
-
-    int min = pow(10, k+1);
-    char* min
-    int max = 0;
-
-    for (int i = 0; i < pow(10, k+1); i++){
-        int temp = i;
-        int* flag = new int[k+1];
-
-
-
-        for(int j = k; j >= 0; j--){
-            arr[j] = temp / (pow(10, j) + 0.5);
-            temp = temp - arr[j] * (pow(10, j) + 0.5);
-            
-            flag[arr[j]] = 1;
+void dfs(int n, int depth, int *check, int *arr, long long *max, long long *min)
+{
+    if (n == depth)
+    {
+        long long result = 0;
+        long long ten = 1;
+        for (int i = n-1; i >= 0; i--)
+        {
+            result += arr[i] * ten;
+            ten = ten * 10;
         }
 
-        int possible = 1;
-
-        for(int j = 0; j < k+1; j++){
-            if (flag[j] != 1){
-                possible = 0;
-                break;
+        if (result < *min)
+        {
+            *min = result;
+            for (int i = 0; i < n; i++)
+            {
+                minarr[i] = '0'+arr[i];
             }
         }
 
-        if (possible == 0){
-            if (i> max)
-                max = i;
-            else
-                min = i;
-            
+        if (result > *max)
+        {
+            *max = result;
+            for (int i = 0; i < n; i++)
+            {
+                maxarr[i] = '0'+arr[i];
+            }
         }
-
-        delete[] flag;
+        return;
     }
 
-    delete[] inequality;
-    delete[] arr;
+    for (int i = 0; i < 10; i++)
+    {
+        int flag = 1;
+        if (check[i] == 0)
+        {
+            check[i] = 1;
+            arr[depth] = i;
 
+            if (depth > 0)
+            {
+                if (str[depth-1] == '<')
+                {
+                    if (arr[depth-1] > arr[depth])
+                        flag = 0;
+                }
+
+                if (str[depth-1] == '>')
+                {
+                    if (arr[depth-1] < arr[depth])
+                        flag = 0;
+                }
+            }
+            
+            if (flag)
+                dfs(n, depth+1, check, arr, max, min);
+            check[i] = 0;
+        }
+    }
 }
 
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str[i];
+    }
+
+    int check[11] = { 0, };
+    int arr[11] = { 0, };
+
+    long long min = 9999999999;
+    long long max = 0;
+    long long *pmin = &min;
+    long long *pmax = &max;
+
+    dfs(n+1, 0, check, arr, pmax, pmin);
+    
+    printf("%s\n", maxarr);
+    printf("%s\n", minarr);
+
+
+}
