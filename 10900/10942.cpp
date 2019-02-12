0ef72cf7aad80d5f15ddd33ca48532485b1d3c03
Jbok <0614bok@gmail.com>
2019-02-12 17:45:51 +0900

    10942 Use DP

---
+++ 10900/10942.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[2000][2000] = { 0, };
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[2000] = { 0, };
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int dif = 0; dif < n; dif++)
+    {
+        int i = 0;
+        int j = dif;
+
+        while (1)
+        {
+            if (j == n)
+                break;
+
+            if (dif == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else if (dif == 1)
+            {
+                if (arr[j] == arr[j-1])
+                    dp[i][j] = 1;
+                else
+                {
+                    dp[i][j] = 0;
+                }
+            }
+            else
+            {
+                if (dp[i+1][j-1] == 1 && arr[j] == arr[j-dif])
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+                
+            }
+            
+            i++;
+            j++;
+        }
+    }
+
+    int m;
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int s, e;
+        scanf("%d", &s);
+        scanf("%d", &e);
+        printf("%d\n", dp[s-1][e-1]);
+    }
+
+}
