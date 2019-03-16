68f33372833012df9fb036ba86cda5ee1de7a783
Jbok <0614bok@gmail.com>
2019-03-16 23:45:33 +0900

    4386

---
+++ 4300/4386.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+double CalValue(double x1, double y1, double x2, double y2)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    
+
+    for (int i = 0; i < n; i++)
+    {
+
+    }
+}
