4f580e184eba2bbbc481b6346f008c7c7cbe3456
Jbok <0614bok@gmail.com>
2019-03-23 16:55:09 +0900

    15686 Brute Force

--- 15600/15686.cpp
+++ 15600/15686.cpp
@@ -1,5 +1,6 @@
+//#include "stdafx.h"
 #include <iostream>
-#include <deque>
+#include <math.h>
 
 using namespace std;
 
@@ -14,80 +15,42 @@ Node chicken[14];
 
 int dir[4][2] = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 } };
 
+Node home[100];
+int homeNum = 0;
+
 int mmin = 50 * 50 * 100;
 
-int bfs(int r, int c, int n, int arr[50][50])
-{
-	int check[50][50] = { 0, };
-
-	deque<Node> temp;
-	temp.push_back({ r, c, 0 });
-	check[r][c] = 1;
-
-
-	while (1)
-	{
-		if (temp.empty())
-			break;
-
-		Node now = temp.front();
-		temp.pop_front();
-
-		if (arr[now.r][now.c] == 2)
-		{
-			return now.cnt;
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			if (now.r + dir[i][0] >= 0 && now.r + dir[i][0] < n && now.c + dir[i][1] >= 0 && now.c + dir[i][1] < n)
-			{
-				if (check[now.r + dir[i][0]][now.c + dir[i][1]] == 0)
-				{
-					Node next = { now.r + dir[i][0], now.c + dir[i][1], now.cnt + 1 };
-					temp.push_back(next);
-					check[now.r + dir[i][0]][now.c + dir[i][1]] = 1;
-				}
-			}
-		}
-
-	}
-}
-
-
-
-
-void Combination(int nn, int n, int c, int target, int index, Node temp[], int arr[50][50])
+void Combination(int nn, int n, int c, int target, int index, Node temp[])
 {
 	if (index == c)
 	{
-		for (int i = 0; i < c; i++)
-		{			
-			arr[temp[i].r][temp[i].c] = 0;
-		}
-
 		int result = 0;
 
-		for (int i = 0; i < nn; i++)
+		for (int i = 0; i < homeNum; i++)
 		{
-			for (int j = 0; j < nn; j++)
+			int resultTemp = 50*50*1000;
+
+			for (int j = 0; j < c; j++)
 			{
-				if (arr[i][j] == 1)
-				{
-					result += bfs(i, j, nn, arr);
-				}
+				int tempR = abs(temp[j].r - home[i].r);
+				
+				int tempC = abs(temp[j].c - home[i].c);
+				
+
+				if (resultTemp > (tempR + tempC))
+					resultTemp = (tempR + tempC);
+				
 			}
+
+			result += resultTemp;
 		}
 
 		if (result < mmin)
 			mmin = result;
+					
 
-
-		for (int i = 0; i < c; i++)
-		{
-			arr[temp[i].r][temp[i].c] = 2;
-		}
 		return;
+	
 	}
 	else if (c - index > n - target)
 	{
@@ -95,10 +58,10 @@ void Combination(int nn, int n, int c, int target, int index, Node temp[], int a
 	}
 	else
 	{
-		Combination(nn, n, c, target + 1, index, temp, arr);
+		Combination(nn, n, c, target + 1, index, temp);
 
 		temp[index] = chicken[target];
-		Combination(nn, n, c, target + 1, index + 1, temp, arr);
+		Combination(nn, n, c, target + 1, index + 1, temp);
 	}
 }
 
@@ -121,12 +84,16 @@ int main()
 			{
 				chicken[chickenNum++] = { i, j };
 			}
+			else if (arr[i][j] == 1)
+			{
+				home[homeNum++] = { i, j };
+			}
 		}
 	}
 
 	Node temp[13] = { {0, }, };
 
-	Combination(n, chickenNum, chickenNum - m, 0, 0, temp, arr);
+	Combination(n, chickenNum, m, 0, 0, temp);
 
 
 	printf("%d\n", mmin);
