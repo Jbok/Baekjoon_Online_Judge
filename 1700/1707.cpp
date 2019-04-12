9c615aee38ccfd4a7a45ab4343b073855a58d5c9
Jbok <0614bok@gmail.com>
2019-04-12 22:11:26 +0900

    1707 BFS

--- 1700/1707.cpp
+++ 1700/1707.cpp
@@ -75,3 +75,124 @@ int main()
 
     }
 }
+
+
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int k;
+	cin >> k;
+	for (int testCase = 0; testCase < k; testCase++)
+	{
+		int flag = 1;
+
+		int v, e;
+		scanf("%d", &v);
+		scanf("%d", &e);
+
+		vector<int> vect[20001];
+
+		for (int i = 0; i < e; i++)
+		{
+			int a, b;
+			scanf("%d", &a);
+			scanf("%d", &b);
+
+			vect[a].push_back(b);
+			vect[b].push_back(a);
+		}
+		
+		
+		int check[20001] = { 0, };
+		int color[20001] = { 0, };
+		
+		int startColor = 1;
+
+		deque<pair<int, int>> dq;
+
+		for (int i = 1; i <= v; i++)
+		{
+			if (check[i] == 1)
+			{
+
+			}
+			else
+			{
+
+				dq.push_back({ i, startColor });
+
+				check[i] = 1;
+				color[1] = startColor;
+
+
+				while (1)
+				{
+					if (dq.empty())
+					{
+						break;
+					}
+
+					pair<int, int> now = dq.front();
+					int nowNode = dq.front().first;
+					int nowColor = dq.front().second;
+					dq.pop_front();
+					//printf("nowNode:%d nowColor:%d \n", nowNode, nowColor);
+
+					while (1)
+					{
+						if (vect[nowNode].empty())
+						{
+							break;
+						}
+
+						int nextNode = vect[nowNode].back();
+						vect[nowNode].pop_back();
+
+						//printf("	nextNode:%d  nextColor:%d \n", nextNode, nowColor*-1);
+
+						if (check[nextNode] == 0)
+						{
+							check[nextNode] = 1;
+							color[nextNode] = nowColor * -1;
+							dq.push_back({ nextNode, nowColor*-1 });
+						}
+						else
+						{
+							if (color[nextNode] == nowColor)
+							{
+								flag = 0;
+							}
+						}
+					}
+				}
+
+
+			}
+		}
+
+		
+
+
+		if (flag == 0)
+		{
+			printf("NO\n");
+		}
+		else
+		{
+			printf("YES\n");
+		}
+
+
+
+
+	}
+}
+
+*/
