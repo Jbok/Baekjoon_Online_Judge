35b0bfed1723b9192ab80ead68817a36c2eb650f
Jbok <0614bok@gmail.com>
2019-04-14 01:00:09 +0900

    1012 DFS

--- 1000/1012.cpp
+++ 1000/1012.cpp
@@ -3,7 +3,27 @@
 
 using namespace std;
 
+int arr[50][50] = { 0, };
+int visit[50][50] = { 0, };
 int dir[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+int m, n, k;
+
+
+void dfs(int r, int c)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		int nextR = r + dir[i][0];
+		int nextC = c + dir[i][1];
+
+		if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m && arr[nextR][nextC] == 1 && visit[nextR][nextC] == 0)
+		{
+			visit[nextR][nextC] = 1;
+			dfs(nextR, nextC);
+		}
+	}
+
+}
 
 int main()
 {
@@ -19,11 +39,19 @@ int main()
 		}
 
 
-		int m, n, k;
 		cin >> m >> n >> k;
 
-		int arr[50][50] = { 0, };
-		int visit[50][50] = { 0, };
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				visit[i][j] = 0;
+				arr[i][j] = 0;
+			}
+		}
+
+		
+
 		
 		for (int i = 0; i < k; i++)
 		{
@@ -32,18 +60,7 @@ int main()
 			arr[y][x] = 1;
 		}
 
-		/*
-		printf("\n");
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				printf("%d ", arr[i][j]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-		*/
+
 		int result = 0;
 
 		for (int i = 0; i < n; i++)
@@ -52,41 +69,12 @@ int main()
 			{
 				if (arr[i][j] == 1 && visit[i][j] == 0)
 				{
-					//printf("i:%d j:%d\n", i, j);
 					result++;
-					deque<pair<int, int>> dq;
-					dq.push_back({ i,j });
-					
-					visit[i][j] = 1;
-
-					while (1)
-					{
-						if (dq.empty())
-						{
-							break;
-						}
-
-						pair<int, int> now = dq.front();
-						dq.pop_front();
-
-						for (int i = 0; i < 4; i++)
-						{
-							int nextR = now.first + dir[i][0];
-							int nextC = now.second + dir[i][1];
-
-							if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m && visit[nextR][nextC] == 0 && arr[nextR][nextC])
-							{
-								//printf("	nextR:%d nextC:%d\n", nextR, nextC);
-								visit[nextR][nextC] = 1;
-								dq.push_back({ nextR, nextC });
-							}
-						}
-
-					}
-
+					dfs(i, j);
 				}
 			}
 		}
+
 
 		printf("%d\n", result);
 
