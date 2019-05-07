14c6216298cf988d7ea8a959fad99a05a2b184d2
Jbok <0614bok@gmail.com>
2019-05-07 20:18:06 +0900

    12100 Simulation

--- 12100/12100.cpp
+++ 12100/12100.cpp
@@ -1,310 +1,245 @@
 #include <iostream>
-#include <fstream>
+#include <cstdio>
 #include <deque>
 
 using namespace std;
 
-// int dir[4][2] = { { 1 , 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+int n;
+int arr[20][20] = { 0, };
+int maxValue = 0;
 
-void up(int n, long long (*arr)[21])
+void Up(int resultArr[][20])
 {
-    for (int k = 0 ; k < n; k++)
-    {
-        for (int i = 0; i < n-1; i++)
-        {
-            for (int j = i; j < n-1; j++)
-            {
-                
-                // if (arr[j][k] == 0)
-                // {
-                int target = j;
-                int move = j+1;
-                while (1)
-                {
-                    if ( move > n-1 || target > n-1)
-                        break;
-                    
-                    if (arr[move][k] == 0 && arr[target][k] != 0)
-                    {
-                        move++;
-                        target++;
-                    }
-                    else if (arr[move][k] == 0 && arr[target][k] == 0)
-                    {
-                        move++;
-                    }
-                    else if (arr[move][k] != 0 && arr[target][k] == 0)
-                    {
-                        arr[target][k] = arr[move][k];
-                        arr[move][k] = 0;
-                        target++;
-                        move++;
-                    }
-                    else if (arr[move][k] != 0 && arr[target][k] != 0)
-                    {
-                        target++;
-                        move++;
-                    }
-                }
-                // }
-                
-                // printf("j: %d k: %d arr[j][k]: %d arr[j+1][k]: %d\n", j, k, arr[j][k], arr[j+1][k]);
-                if (arr[j][k] == arr[j+1][k])
-                {
-                    arr[j][k] = 2*arr[j][k];
-                    arr[j+1][k] = 0;
-                }
-            }
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		deque<int> dq;
+		deque<int> destDq;
+
+		for (int j = 0; j < n; j++)
+		{
+			if (resultArr[j][i] != 0)
+			{
+				dq.push_back(resultArr[j][i]);
+			}
+		}
+
+		for (deque<int>::iterator iter = dq.begin(); iter != dq.end(); iter++)
+		{
+			if ((iter +1) != dq.end() && *iter == *(iter + 1))
+			{
+				destDq.push_back((*iter) * 2);
+				iter++;
+			}
+			else
+			{
+				destDq.push_back((*iter));
+			}
+		}
+
+		int idx = 0;
+		for (deque<int>::iterator iter = destDq.begin(); iter != destDq.end(); iter++)
+		{
+			resultArr[idx++][i] = (*iter);
+		}
+		for (int j = idx; j < n; j++)
+		{
+			resultArr[j][i] = 0;
+		}
+	}
 }
 
-void left(int n, long long (*arr)[21])
+void Down(int resultArr[][20])
 {
-    for (int k = 0 ; k < n; k++)
-    {
-        for (int i = 0; i < n-1; i++)
-        {
-            for (int j = i; j < n-1; j++)
-            {
+	for (int i = 0; i < n; i++)
+	{
+		deque<int> dq;
+		deque<int> destDq;
 
-                int target = j;
-                int move = j+1;
-                while (1)
-                {
-                    if ( move > n-1 || target > n -1 )
-                        break;
-                    
-                    if (arr[k][move] == 0 && arr[k][target] != 0)
-                    {
-                        move++;
-                        target++;
-                    }
-                    else if (arr[k][move] == 0 && arr[k][target] == 0)
-                    {
-                        move++;
-                    }
-                    else if (arr[k][move] != 0 && arr[k][target] == 0)
-                    {
-                        arr[k][target] = arr[k][move];
-                        arr[k][move] = 0;
-                        target++;
-                        move++;
-                    }
-                    else if (arr[k][move] != 0 && arr[k][target] != 0)
-                    {
-                        target++;
-                        move++;
-                    }
+		for (int j = n-1; j >= 0; j--)
+		{
+			if (resultArr[j][i] != 0)
+			{
+				dq.push_back(resultArr[j][i]);
+			}
+		}
 
-                }
+		for (deque<int>::iterator iter = dq.begin(); iter != dq.end(); iter++)
+		{
+			if ((iter + 1) != dq.end() && *iter == *(iter + 1))
+			{
+				destDq.push_back((*iter) * 2);
+				iter++;
+			}
+			else
+			{
+				destDq.push_back((*iter));
+			}
+		}
 
-                
-                if (arr[k][j] == arr[k][j+1])
-                {
-                    arr[k][j] = 2*arr[k][j];
-                    arr[k][j+1] = 0;
-                }
-            }
-        }
-    }
+		int idx = n-1;
+		for (deque<int>::iterator iter = destDq.begin(); iter != destDq.end(); iter++)
+		{
+			resultArr[idx--][i] = (*iter);
+		}
+		for (int j = idx; j >= 0; j--)
+		{
+			resultArr[j][i] = 0;
+		}
+	}
 }
 
-void down(int n, long long (*arr)[21])
+void Right(int resultArr[][20])
 {
-    for (int k = 0; k < n; k++)
-    {
-        for (int i = n - 1; i > 0; i--)
-        {
-            for (int j = i; j > 0; j--)
-            {
-                if (arr[j][k] == 0)
-                {
-                    int target = j;
-                    int move = j-1;
-                    while (1)
-                    {
-                        if ( move < 0 || target < 0 )
-                            break;
-                        
-                        if (arr[move][k] == 0 && arr[target][k] != 0)
-                        {
-                            move--;
-                            target--;
-                        }
-                        else if (arr[move][k] == 0 && arr[target][k] == 0)
-                        {
-                            move--;
-                        }
-                        else if (arr[move][k] != 0 && arr[target][k] == 0)
-                        {
-                            arr[target][k] = arr[move][k];
-                            arr[move][k] = 0;
-                            target--;
-                            move--;
-                        }
-                        else if (arr[move][k] != 0 && arr[target][k] != 0)
-                        {
-                            target--;
-                            move--;
-                        }
+	for (int i = 0; i < n; i++)
+	{
+		deque<int> dq;
+		deque<int> destDq;
 
-                    }
-                }
-                
-                if (arr[j][k] == arr[j-1][k])
-                {
-                    arr[j][k] = 2*arr[j][k];
-                    arr[j-1][k] = 0;
-                }
-            }
-        }
-    }
+		for (int j = n - 1; j >= 0; j--)
+		{
+			if (resultArr[i][j] != 0)
+			{
+				dq.push_back(resultArr[i][j]);
+			}
+		}
+
+		for (deque<int>::iterator iter = dq.begin(); iter != dq.end(); iter++)
+		{
+			if ((iter + 1) != dq.end() && *iter == *(iter + 1))
+			{
+				destDq.push_back((*iter) * 2);
+				iter++;
+			}
+			else
+			{
+				destDq.push_back((*iter));
+			}
+		}
+
+		int idx = n - 1;
+		for (deque<int>::iterator iter = destDq.begin(); iter != destDq.end(); iter++)
+		{
+			resultArr[i][idx--] = (*iter);
+		}
+		for (int j = idx; j >= 0; j--)
+		{
+			resultArr[i][j] = 0;
+		}
+	}
 }
 
-void right(int n, long long (*arr)[21])
+void Left(int resultArr[][20])
 {
-    for (int k = 0; k < n; k++)
-    {
-        for (int i = n - 1; i > 0; i--)
-        {
-            for (int j = i; j > 0; j--)
-            {
-                if (arr[k][j] == 0)
-                {
-                    int target = j;
-                    int move = j-1;
-                    while (1)
-                    {
-                        if ( move < 0 || target < 0 )
-                        break;
-                    
-                        if (arr[k][move] == 0 && arr[k][target] != 0)
-                        {
-                            move--;
-                            target--;
-                        }
-                        else if (arr[k][move] == 0 && arr[k][target] == 0)
-                        {
-                            move--;
-                        }
-                        else if (arr[k][move] != 0 && arr[k][target] == 0)
-                        {
-                            arr[k][target] = arr[k][move];
-                            arr[k][move] = 0;
-                            target--;
-                            move--;
-                        }
-                        else if (arr[k][move] != 0 && arr[k][target] != 0)
-                        {
-                            target--;
-                            move--;
-                        }
-                    }
-                }
-                
-                if (arr[k][j] == arr[k][j-1])
-                {
-                    arr[k][j] = 2*arr[k][j];
-                    arr[k][j-1] = 0;
-                }
-            }
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		deque<int> dq;
+		deque<int> destDq;
+
+		for (int j = 0; j < n; j++)
+		{
+			if (resultArr[i][j] != 0)
+			{
+				dq.push_back(resultArr[i][j]);
+			}
+		}
+
+		for (deque<int>::iterator iter = dq.begin(); iter != dq.end(); iter++)
+		{
+			if ((iter + 1) != dq.end() && *iter == *(iter + 1))
+			{
+				destDq.push_back((*iter) * 2);
+				iter++;
+			}
+			else
+			{
+				destDq.push_back((*iter));
+			}
+		}
+
+		int idx = 0;
+		for (deque<int>::iterator iter = destDq.begin(); iter != destDq.end(); iter++)
+		{
+			resultArr[i][idx++] = (*iter);
+		}
+		for (int j = idx; j < n; j++)
+		{
+			resultArr[i][j] = 0;
+		}
+	}
+}
+
+void Combination(int now, int end, int *tArr)
+{
+	if (now == end)
+	{
+		int resultArr[20][20] = { 0, };
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				resultArr[i][j] = arr[i][j];
+			}
+		}
+
+		for (int i = 0; i < 5; i++)
+		{
+			if (tArr[i] == 0)
+			{
+				Up(resultArr);
+			}
+			else if (tArr[i] == 1)
+			{
+				Down(resultArr);
+			}
+			else if (tArr[i] == 2)
+			{
+				Right(resultArr);
+			}
+			else if (tArr[i] == 3)
+			{
+				Left(resultArr);
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (resultArr[i][j] > maxValue)
+				{
+					maxValue = resultArr[i][j];
+				}
+			}
+		}
+		
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			tArr[now] = i;
+			Combination(now + 1, end, tArr);
+		}
+	}
+	
 }
 
 int main()
 {
-    string filePath = "log.txt";
+	cin >> n;
 
-    ofstream writeFile(filePath.data());
-    int n;
-    cin >> n;
-    
-    long long max = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			scanf("%d", &arr[i][j]);
+		}
+	}
 
-    deque<long long> dq;
-
-    long long cnt = 0;
-    dq.push_back(cnt);
-
-    for (int i = 0 ; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            int temp;
-            cin >> temp;
-            dq.push_back(temp);
-        }
-    }
-
-    while (1)
-    {
-        cnt = dq.front();
-        dq.pop_front();
-
-        if (cnt > 5)
-            break;
-
-        long long arr[21][21];
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                arr[i][j] = dq.front();
-                dq.pop_front();
-                if (max < arr[i][j])
-                    max = arr[i][j];
-            }
-        }
-
-        for (int k = 0; k < 4; k++)
-        {
-            long long tarr[21][21];
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    tarr[i][j] = arr[i][j];
-                }
-            }
-
-            if (k == 0)
-            {
-                up(n, tarr);
-            }
-            else if (k == 1)
-            {
-                down(n, tarr);
-            }
-            else if (k == 2)
-            {
-                left(n, tarr);
-            }
-            else 
-            {
-                right(n, tarr);
-            }
-
-            dq.push_back(cnt+1);
-
-            writeFile << "\n=============cnt: "<< cnt-1 << " k:" << k << "\n";
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    writeFile << tarr[i][j] << " ";
-                    //printf("%d ", tarr[i][j]);
-                    dq.push_back(tarr[i][j]);
-                }
-                writeFile << "\n";
-            }
-            writeFile << "\n\n";
-        }
-    }
-
-    writeFile.close();
-
-    printf("%d\n", max);
+	
+	int tArr[5] = { 0, };
+	Combination(0, 5, tArr);
+	
+	printf("%d\n", maxValue);
 }
